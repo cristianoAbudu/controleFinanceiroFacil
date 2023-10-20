57cd6f31e4cf65b1b58a8c48bd19ff3eb4164d9c
jovem tranquilao <jovem.tranquilao.2023@gmail.copm>
2023-10-20 19:40:21 -0300

    Tela inicial pronta

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  TextInput,
+  Button,
+  Alert
+} from 'react-native';
+
+import React, {useState} from 'react';
+
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="R$"
+        keyboardType="numeric"
+      />
+      <Text>Categoria:</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
+      <TextInput
+        placeholder="Descrição"
+      />
+      <Button
+        title="OK"
+        onPress={() => Alert.alert('OK')}
+      />
     </View>
   );
 }
